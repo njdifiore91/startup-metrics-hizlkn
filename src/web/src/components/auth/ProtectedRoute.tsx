a380d782d2019906795282ceeb38b0d95f907c4b
njdifiore91 <nick@blitzy.com>
2025-01-13 20:48:50 -0500

    BugFix: Automated bug fixes for src/web/src/components/auth/ProtectedRoute.tsx

--- src/web/src/components/auth/ProtectedRoute.tsx
+++ src/web/src/components/auth/ProtectedRoute.tsx
@@ -7,9 +7,9 @@
 
 import React, { FC, PropsWithChildren, useEffect, memo } from 'react';
 import { Navigate } from 'react-router-dom';
-import { useAuth } from '../../hooks/useAuth';
-import LoadingSpinner from '../common/LoadingSpinner';
-import { theme } from '../../config/theme';
+import { useAuth } from '../../hooks/useAuth.js';
+import LoadingSpinner from '../common/LoadingSpinner.js';
+import { theme } from '../../config/theme.js';
 
 // Route constants
 const LOGIN_ROUTE = '/login';
