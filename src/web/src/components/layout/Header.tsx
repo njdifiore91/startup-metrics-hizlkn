1ce1fd8f804faf66cc11784a64e219c2967a9552
njdifiore91 <nick@blitzy.com>
2025-01-17 20:34:33 -0500

    BugFix: Automated bug fixes for src/web/src/components/layout/Header.tsx

--- src/web/src/components/layout/Header.tsx
+++ src/web/src/components/layout/Header.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom'; // v6.0.0
 import { useMetrics } from '../../hooks/useMetrics';
-import ProfileMenu from '../user/ProfileMenu';
+import { ProfileMenu } from '../user/ProfileMenu';
 import { Button } from '../common/Button';
 import ErrorBoundary from '../common/ErrorBoundary';
 import logo from '../../assets/images/logo.svg';
@@ -117,7 +117,7 @@ export const Header: React.FC<HeaderProps> = React.memo(({
           />
         </div>
 
-        <style jsx>{`
+        <style>{`
           .header {
             position: fixed;
             top: 0;
