9520a7228dc243e0327a254be378a9ec5d823950
njdifiore91 <nick@blitzy.com>
2025-01-17 21:36:04 -0500

    BugFix: Automated bug fixes for src/web/src/components/common/Toast.tsx

--- src/web/src/components/common/Toast.tsx
+++ src/web/src/components/common/Toast.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useCallback, useRef } from 'react';
 import classNames from 'classnames'; // ^2.3.2
 import { useSwipeable } from 'react-swipeable'; // ^7.0.0
 import { ToastType, ToastPosition } from '../../hooks/useToast';
-import { animations } from '../../styles/animations.css';
+import '../../styles/animations.css';
 import ErrorBoundary from './ErrorBoundary';
 
 // Props interfaces
@@ -78,6 +78,7 @@ const Toast: React.FC<ToastProps> = React.memo(({
   const swipeHandlers = useSwipeable({
     onSwipedLeft: () => !rtl && onClose(id),
     onSwipedRight: () => rtl && onClose(id),
+    preventDefaultTouchmoveEvent: true,
     trackMouse: true
   });
 
